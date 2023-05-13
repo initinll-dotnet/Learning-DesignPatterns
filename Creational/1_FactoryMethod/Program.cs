f4ba9b553d4bbbacfa488b2569dd3d9e1580ca53
Nitin Londhe <nitin.londhe@genmills.com>
2023-05-13 17:41:20 +0530

    added definition

--- Creational/1_FactoryMethod/Program.cs
+++ Creational/1_FactoryMethod/Program.cs
@@ -1,3 +1,11 @@
 ﻿using _1_FactoryMethod.Client;
 
+using System.Diagnostics.Metrics;
+
 new Client().Main();
+
+/* 
+Factory Method is a creational design pattern that provides
+an interface for creating objects in a superclass, but allows
+subclasses to alter the type of objects that will be created.
+*/
